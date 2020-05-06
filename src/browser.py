d374a95c927d0fd17bba882c3601c6fb1fe08f63
Patience Mpofu <pgmpofu@gmail.com>
2020-05-06 13:40:36 -0700

    Add additional checks to prevent file from opening when empty

--- src/browser.py
+++ src/browser.py
@@ -2,17 +2,23 @@ import os
 import webbrowser
 
 
-def open_website():
-    if os.path.exists('websites.txt'):
+class BrowserManager:
+
+    def __init__(self):
+        pass
+
+
+def open_website(file_to_open : str, error_msg: str):
+    if os.path.exists(file_to_open) and os.path.getsize(file_to_open) > 0:
         try:
             with open('websites.txt') as read_obj:
                 for num, url in enumerate(read_obj):
                     webbrowser.open_new_tab(url.strip())
-        except Exception as e:
-            print ('An error occurred ', e)
+        except IOError as e:
+            print (error_msg, e)
     else:
-        print ('File does not exist, please add and retry')
+        raise IOError(error_msg)
 
 
 if __name__ == '__main__':
-    open_website()
+    open_website('websites.txt', 'File does not exist or is empty, please add and retry')
